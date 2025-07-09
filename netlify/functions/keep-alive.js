d64197feade46d54d9ddca3aca56ed7eda148a5c
itElectroNet <stephennickalls@gmail.com>
2025-07-09 15:43:42 +1200

    OMG!!

--- netlify/functions/keep-alive.js
+++ netlify/functions/keep-alive.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const handler = async (event, context) => {
-  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseUrl = process.env.supabaseUrl;
   const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.supabaseKey;
   
   if (!supabaseUrl || !supabaseKey) {
